51f029dd204c231906c109107ffa3bcd3672d9f6
jacb <>
2023-10-23 17:40:16 +0800

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -12,27 +12,6 @@ const config = {
     defaultNetwork: "basegoerli",
     solidity: { version: "0.8.4", settings: { optimizer: { enabled: true, runs: 100 } } },
     networks: {
-        arbitrum: {
-            currency: "ETH",
-            currencyDecimals: 18,
-            url: "https://arb-mainnet.g.alchemy.com/v2/K6ooygeuX_XZYGZz1QqHyYMiFE9p3elH",
-            explorer: "https://arbiscan.io",
-            rpc: "https://arbitrum-mainnet.infura.io",
-            accounts: getAccounts("arbitrum"),
-            chainId: 42161,
-            // gasPrice: 0.1 * 1e9,
-            //gas: 950000,
-        },
-
-        arbigoerli: {
-            currency: "ETH",
-            currencyDecimals: 18,
-            url: "https://arb-goerli.g.alchemy.com/v2/sFQLzqNwFsZKsi3kz0zy-4b1AFaEm-xg",
-            explorer: "https://goerli.arbiscan.io",
-            rpc: "https://goerli-rollup.arbitrum.io/rpc",
-            accounts: getAccounts("arbigoerli"),
-            chainId: 421613,
-        },
         base: {
             currency: "ETH",
             currencyDecimals: 18,
---
+++ readme.md
@@ -0,0 +1,17 @@
+# social shares client
+
+## init
+
+```shell
+# copy env-example.js to env.js, replace keys/address in env.js
+cp env-example.js env.js
+
+# install required libraries
+npm i
+
+# run example in base testnet
+npx hardhat run main.js --network basegoerli
+
+# run example in base mainnet
+npx hardhat run main.js --network base
+```
